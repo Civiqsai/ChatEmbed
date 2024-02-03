b9f7435a06004fe5862c1920e99b656ab7dd3d98
Daniel <daniel.verloop@tappstr.com>
2024-02-03 11:11:07 +0100

    Changed Tappstr to CiviQ

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -53,14 +53,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://tappstr.com'}
+        href={'https://civiq.ai'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Tappstr</span>
+        <span> CiviQ</span>
       </a>
     </span>
   );
