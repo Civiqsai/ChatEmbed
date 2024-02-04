e4af4398f399adccc035e136c892e62513c95925
Daniel <daniel.verloop@tappstr.com>
2024-02-04 01:01:03 +0100

    Civiqss

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -60,7 +60,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Civiqs</span>
+        <span> CiviQs</span>
       </a>
     </span>
   );
